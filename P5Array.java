052ccba62fb5f387e4be27dbf907035574a17739
Unknown <AaronAvila09@gmail.com>
2017-09-18 01:51:27 -0700

    finished program

--- P5Array.java
+++ P5Array.java
@@ -1,6 +1,11 @@
+/*
+Aaron Avila
+*/
+
 import java.util.*;
 
 class SortArray{
+
 	//attributes
 	String a;
 	int [] iArr;
@@ -10,6 +15,10 @@ class SortArray{
 		a = readNumbers();
 		String [] stArr = makeSArray(a);
 		iArr = convertIntArr(stArr);
+
+		//selectionSort(iArr);
+		//bubbleSort(iArr);
+		insertionSort(iArr);
 	}//constructor
 
 	static String readNumbers(){
@@ -45,5 +54,48 @@ class SortArray{
 
 		return iArr;
 	}//convertIntArr
+
+	//selectionSort
+	static void selectionSort(int[] arr){
+		//Sort the array arr[]
+		for (int i = 0; i < arr.length - 1; i++) { //outer
+			int index = i;
+				for (int j = i + 1; j < arr.length; j++)
+					if (arr[j] < arr[index]) index = j;    
+				int smallerNumber = arr[index]; 
+				arr[index] = arr[i];
+				arr[i] = smallerNumber;
+		}//for i
+	}//selectionSort
+
+	//bubbleSort
+	static void bubbleSort(int[] arr){
+		//Sort the array arr[]
+		for (int i = 0; i<  arr.length - 1; i++) {
+			for (int j = 1; j <  arr.length - i; j++) {
+				if (arr[j-1] > arr[j]) {
+					//swap(j-1, j)
+					int temp = arr[j-1];
+					arr[j-1] = arr[j];
+					arr[j] = temp;
+				}//if
+			} //for j
+		}//for i
+	}//bubbleSort
+
+	//insertionSort
+	static void insertionSort(int[] arr){
+		//Sort the array arr[]
+		for (int i = 1; i < arr.length; i++) { //outer loop
+			for(int j = i ; j > 0 ; j--){    //inner loop
+				if(arr[j] < arr[j-1]){ 
+					//swap (array at j & j-1)
+					int temp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = temp;
+				}//if
+			}//for j
+		} //for i
+	}//insertionSort
 
 }//class
