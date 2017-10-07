6f9b3a2e87fa7a20663a607013f8540129d3a2da
sonicboom0075 <AaronAvila09@gmail.com>
2017-10-06 21:35:13 -0700

    Add files via upload
    Modifications to fit the requirements for Project 1.

--- P5.java
+++ P5.java
@@ -7,10 +7,21 @@ import java.util.*;
 class Test {
 
 	public static void main(String [] args){
-		SortArray sobj = new SortArray();
-
-		System.out.println(sobj.a);
-		System.out.println(Arrays.toString(sobj.iArr));
+		System.out.println("\nInsertion Sort:\n");
+		SortArray obj = new SortArray();
+		obj.insertionSort();
+		System.out.println("\n\nOriginal= " +obj.a +"\nSorted= " +obj);
+		
+		System.out.println("\nBubble Sort:");
+		SortArray obj2 = new SortArray("         45   456 12 3 32    ");
+		obj2.bubbleSort();
+		System.out.println("\n\nOriginal= " +obj2.a +"\nSorted= " +obj2);
+		
+		System.out.println("\nSelection Sort:");
+		SortArray obj3 = new SortArray(new int[]{22,18,28,11});
+		obj3.selectionSort();
+		System.out.println("\n\nOriginal= " +obj3.a +"\nSorted= " +obj3);
+		
 	}//main
 
 }//class
